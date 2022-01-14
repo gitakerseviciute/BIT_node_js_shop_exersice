daa9e92f185c8f757d38a421d22f0ee4ca5a779a
gitakerseviciute <58921250+gitakerseviciute@users.noreply.github.com>
2022-01-14 19:16:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+UZDUOTIS:
+- perskaityti visu produktu failus;
+- susideti visus produktus i viena bendra masyva;
+- isspausdinti produktu lentele, kuri atordys taip (zr. zemiau)
+"Univermagas" pardavime turi:
+-----------------------------
+1) Prekes pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+2) Prekes pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+3) Prekes pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+-----------------------------
+Parduotuves suvestine:
+- turimu prekiu sandelyje: [total kiekis]
+- parduotu prekiu: [total kiekis]
+- suprekiauta suma: [total pinigu] [valiuta]
+- galimu pardavimu: [total pinigu] [valiuta]
+- maksimalus galima parduotuves apyvarta: [total pinigu] [valiuta]
+*/
+
